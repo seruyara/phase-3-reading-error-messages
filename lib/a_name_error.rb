36a2f4646d3851d051de54b216cfc792864869dd
Sheila Seruya <Sheilaseruya@gmail.com>
2023-05-15 13:41:09 +0300

    done

--- lib/a_name_error.rb
+++ lib/a_name_error.rb
@@ -1,3 +1,5 @@
-"hello world"
+# ./lib/a_name_error.rb
 
-hello_world
+undefined_variable = "Hello, World!"  # Define the variable with an appropriate value
+puts undefined_variable  # Output: Hello, World!
+
